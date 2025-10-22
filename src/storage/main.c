5604d8c7af3b90016c475abf56b73808883c258f
Matvey Pavlich <matveypavlich@gmail.com>
2025-10-22 22:05:13 +0100

    feat: implement basic read-evaluate-print loop

--- src/storage/main.c
+++ src/storage/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_INPUT_SIZE 1024
+ 
 int main(void)
 {
-        printf("Hellow world\n");
+        char input[MAX_INPUT_SIZE];
+
+        printf("matthewdb >");
+        while (fgets(input, sizeof(input), stdin)) {
+                // remove trailing newline
+                input[strcspn(input, "\n")] = '\0';
+
+                if (strcmp(input, ".exit") == 0) {
+                        break;
+                } else {
+                        printf("ERROR: '%s' is unrecognised command\n", input);
+                }
+
+                printf("matthewdb >");
+        }
+
         return 0;
 }
