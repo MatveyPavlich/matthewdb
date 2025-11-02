cd420572ebac3c8c9ac34cc4fcda5e32210e0d0b
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-02 10:37:11 +0000

    feat: small function to open and write some data with open() and write()

--- lab/main.c
+++ lab/main.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-// #include <fcntl.h>    // open
-// #include <unistd.h>   // read, write, close
-// #include <sys/stat.h> // file permission
+#include <fcntl.h>     // open()
+#include <unistd.h>    // write(), close()
+#include <sys/stat.h>  // S_IRUSR, S_IWUSR
+#include <stdio.h>     // perror()
+#include <stdlib.h>    // exit()
 
-/*
-int open_db(const char *filename)
-{
-    int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-    return fd;
-}
-*/
+int main(void) {
+        // Open (or create) the database file
+        int my_file = open("./build/mk.db", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        if (my_file == -1) {
+                perror("Db file could not be opened");
+                exit(EXIT_FAILURE);
+        }
 
-int main(void)
-{
-        FILE *my_file;
-        my_file = fopen("./build/test.txt", "w");
-        fclose(my_file);
+        // Data to write
+        const char *data = "Yoo\n";
+        ssize_t bytes_written = write(my_file, data, 4);  // 4 = length of "Yoo\n"
+        if (bytes_written == -1) {
+                perror("Write failed");
+                close(my_file);
+                exit(EXIT_FAILURE);
+        }
 
+        // Always close the file
+        close(my_file);
         return 0;
 }
