781ea4e75bb1a1f540c5a0fb5eadc563d4388429
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-02 08:55:19 +0000

    feat: create mk.db file

--- src/storage/main.c
+++ src/storage/main.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <fcntl.h>    // open
+#include <unistd.h>   // read, write, close
+#include <sys/stat.h> // file permission
+
 #define MAX_INPUT_SIZE 1024
- 
+
+
+int open_db(const char *filename)
+{
+    int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    return fd;
+}
+
+
 int main(void)
 {
         char input[MAX_INPUT_SIZE];
@@ -15,6 +31,9 @@ int main(void)
 
                 if (strcmp(input, ".exit") == 0) {
                         break;
+                } if (strcmp(input, "db") == 0) {
+                        open_db("./build/mk.db");
+                        printf("mk.db created\n");
                 } else {
                         printf("ERROR: '%s' is unrecognised command\n", input);
                 }
